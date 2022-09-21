5d546353a9b5fd4eb26edc17599fc6caa8af7747
yogeshbiradar123 <114044104+yogeshbiradar123@users.noreply.github.com>
2022-09-21 12:11:50 +0530

    Add files via upload

---
+++ IfElsetest.java
@@ -0,0 +1,20 @@
+package Day1;
+
+public class IfElsetest {
+
+	public static void main(String[] args) {
+		int y=10;
+		
+		 if( y==10)		
+		 {  
+			 System.out.println("welcome");
+			 System.out.println("welcome");
+		 }   		 
+      	 else if( y>5) 		System.out.println("hello");
+		
+		 else  if(y<20)		System.out.println("sorry");
+		
+
+	}
+
+}
