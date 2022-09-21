d5a91ecd0fd92650f009de1ac9d3e460c7d4a200
yogeshbiradar123 <114044104+yogeshbiradar123@users.noreply.github.com>
2022-09-21 12:07:47 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,22 @@
+package Day1;
+//write a program to print hello message 
+// to make desktop application we should make main method 
+//System -- predefined class 
+//out  -- stream
+//println -- method 
+//library
+
+class Student 
+{
+	public static void main(String[] args) {
+
+		System.out.println("hello1");
+		System.out.println("hello2");
+		System.out.println("hello3");
+		System.out.print("hello4");
+		System.out.print("hello5");
+		
+
+	}
+
+}
